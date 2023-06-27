347ef1bea0ea8b753ab278a0d582dba6d7e5c2cb
Ingrid Madrigal <39933650+imadrig@users.noreply.github.com>
2023-06-27 11:02:18 -0400

    Initial Commit and completed exercise 1

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var answer = 4;
+            var response = (answer < 9) ? "is less than 9" : "is grater than or equal to 9";
+
+            Console.WriteLine($"{answer} {response}");
+
         }
     }
 }
